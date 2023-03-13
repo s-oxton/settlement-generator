709fb3e0f641878b77e3a72f1af21d8a2a7055c3
s-oxton <b8009477@ncl.ac.uk>
2023-03-13 23:13:43 +0000

    Building Types
    Cleaned up house placing code, added 2 building types in addition to regular houses

--- Settlement_Generator/Assets/Scripts/HousePlacer.cs
+++ Settlement_Generator/Assets/Scripts/HousePlacer.cs
@@ -11,8 +11,6 @@ public class HousePlacer : MonoBehaviour
         private GameObject house;
         //how close this house can be to other houses
         private float houseSeparation;
-        //list of nearby houses
-        private List<House> nearbyHouses = new List<House>();
 
         //Initialiser
         public House(GameObject thisHouse, float separation)
@@ -41,28 +39,13 @@ public class HousePlacer : MonoBehaviour
             house.transform.position = house.transform.position + movement;
         }
 
-        public void ClearNearbyHouses()
-        {
-            nearbyHouses.Clear();
-        }
-
-        public void AddNearbyHouse(House nearbyHouse)
-        {
-            nearbyHouses.Add(nearbyHouse);
-        }
-
-        public void DebugHouseList()
-        {
-            Debug.Log("Number of nearby houses: " + nearbyHouses.Count);
-        }
-
     }
 
     [SerializeField]
     private GameObject housePrefab;
 
     [SerializeField]
-    [Range(2, 60)]
+    [Range(2, 600)]
     private int noOfHouses = 10;
 
     [SerializeField]
@@ -91,46 +74,61 @@ public class HousePlacer : MonoBehaviour
         {
             GameObject house;
             house = Instantiate(housePrefab, CreateRandomLocation(), Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), gameObject.transform);
-            //add created house to array of all houses
-            houseArray[i] = new House(house, houseSeparation);
-        }
-
-    }
-
-    private void Update()
-    {
-
-
-
-        //detect nearby houses
-        //for every house in the array
-        foreach (House house in houseArray)
-        {
-            //clear the list of nearby houses
-            //house.ClearNearbyHouses();
-
-            //loop through all the other houses
-            foreach (House comparisonHouse in houseArray)
+            //make 1 in 20 houses big
+            int houseType = Random.Range(0, 100);
+            if (houseType == 0)
             {
-                if (house != comparisonHouse)
-                {
-                    //find the distance between the houses
-                    float distance = Vector3.Distance(comparisonHouse.GetPosition(), house.GetPosition());
-                    //if the houses are too close, add comparison house to first house's list of houses that are too close
-                    if (distance < house.GetSeparation())
-                    {
-                        //house.AddNearbyHouse(comparisonHouse);
-                        //get direction between the two houses
-                        Vector3 direction = -(house.GetPosition() - comparisonHouse.GetPosition()).normalized;
-                        //Debug.DrawRay(comparisonHouse.GetPosition(), direction, Color.green, Time.deltaTime);
-                        //move the other house 
-                        comparisonHouse.MoveHouse(direction * houseMoveDistance);
-                    }
-                }
+                Debug.Log("Tavern");
+                house.transform.localScale = house.transform.localScale * 2;
+                //shift tavern to be closer to the middle
+                house.transform.position = new Vector3(house.transform.position.x/2, house.transform.position.y + 0.25f, house.transform.position.z/2);
+                houseArray[i] = new House(house, 1.2f);
+            }
+            else if (houseType == 1)
+            {
+                Debug.Log("Wizard Tower");
+                house.transform.localScale = new Vector3(1f, 4f, 1f);
+                //shift wizard tower to be closer to the middle
+                house.transform.position = new Vector3(house.transform.position.x / 2, house.transform.position.y + 0.75f, house.transform.position.z / 2);
+                houseArray[i] = new House(house, 3f);
+            }
+            else
+            {
+                //add created house to array of all houses
+                houseArray[i] = new House(house, houseSeparation);
             }
 
-            //house.DebugHouseList();
+        }
 
+        //spread out position of houses 20 times
+        //if they're not spread out after 20 times just give up its not worth it let it go
+        for (int i = 0; i < 20; i++)
+        {
+            //detect nearby houses
+            //for every house in the array
+            foreach (House house in houseArray)
+            {
+                //loop through all the other houses
+                foreach (House comparisonHouse in houseArray)
+                {
+                    if (house != comparisonHouse)
+                    {
+                        //find the distance between the houses
+                        float distance = Vector3.Distance(comparisonHouse.GetPosition(), house.GetPosition());
+                        //if the houses are too close, add comparison house to first house's list of houses that are too close
+                        if (distance < house.GetSeparation())
+                        {
+                            //get direction between the two houses
+                            Vector3 direction = -(house.GetPosition() - comparisonHouse.GetPosition()).normalized;
+                            //Debug.DrawRay(comparisonHouse.GetPosition(), direction, Color.green, Time.deltaTime);
+                            //move the other house 
+                            direction = new Vector3(direction.x, 0f, direction.z);
+                            comparisonHouse.MoveHouse(direction * houseMoveDistance);
+                        }
+                    }
+                }
+
+            }
         }
 
     }
